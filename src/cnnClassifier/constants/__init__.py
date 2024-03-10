f86f6b5812120e2334e24d8b63e889870833cb2c
abhijeetGithu <abhijeetas8660211@gmail.com>
2024-03-10 15:13:25 +0530

    added remaining files

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"C:\Users\Lenovo\Desktop\ml_projects\chicken-disease-model\chicken-disease-classifcation\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\Lenovo\Desktop\ml_projects\chicken-disease-model\chicken-disease-classifcation\params.yaml")
